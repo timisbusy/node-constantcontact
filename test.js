3e70964eee40c8ec172126403b5988919e2c41e5
Tim Atkinson <timisbusy@gmail.com>
2013-09-17 14:49:27 -0400

    bugfixes, testing pagination

--- test.js
+++ test.js
@@ -3,7 +3,17 @@ var client = require('./index')
 
 client.useKey(conf.apiKey);
 client.useToken(conf.testToken);
-client.campaigns.get(function (err, res) {
+
+client.contacts.get({ limit: 1 }, logResultAndFollow);
+
+// checking out pagination links
+
+function logResultAndFollow (err, result) {
   if (err) { throw err; }
-  console.log(res);
-});
+  console.log(result);
+  if (result.meta && result.meta.pagination && result.meta.pagination.next_link) {
+    client.followPagination(result.meta.pagination.next_link, logResultAndFollow); // recurs to add next page of results.
+  } else {
+    return console.log('done!');
+  }
+}
